a376f8bcfa682a98d9ba60f52d14de50123201bb
nicemama82-afk <nicemama82@gmail.com>
2026-05-03 03:57:22 +1000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Navigation from './components/Navigation';
-import Footer from './components/Footer';
-
 import HomePage from './pages/HomePage';
-import MarketplacePage from './pages/MarketplacePage';
-import PricingPage from './pages/PricingPage';
-import CreatorsPage from './pages/CreatorsPage';
-import BlogPage from './pages/BlogPage';
-import ContactPage from './pages/ContactPage';
-import DisclaimerPage from './pages/DisclaimerPage';
-import TermsPage from './pages/TermsPage';
-import PrivacyPage from './pages/PrivacyPage';
-
-import './App.css';
 
 function App() {
   return (
     <Router>
-      <Navigation />
-
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/marketplace" element={<MarketplacePage />} />
-        <Route path="/pricing" element={<PricingPage />} />
-        <Route path="/creators" element={<CreatorsPage />} />
-        <Route path="/blog" element={<BlogPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/disclaimer" element={<DisclaimerPage />} />
-        <Route path="/terms" element={<TermsPage />} />
-        <Route path="/privacy" element={<PrivacyPage />} />
       </Routes>
-
-      <Footer />
     </Router>
   );
 }
