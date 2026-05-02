79ae428c5327d72f087ff16ee4423e253d913b56
nicemama82-afk <nicemama82@gmail.com>
2026-05-02 13:56:51 +1000

    Refactor App component structure and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
+
 import HomePage from './pages/HomePage';
 import MarketplacePage from './pages/MarketplacePage';
 import PricingPage from './pages/PricingPage';
@@ -10,28 +12,27 @@ import ContactPage from './pages/ContactPage';
 import DisclaimerPage from './pages/DisclaimerPage';
 import TermsPage from './pages/TermsPage';
 import PrivacyPage from './pages/PrivacyPage';
+
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-gradient-to-br from-dark via-accent to-dark text-white flex flex-col">
-        <Navigation />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/marketplace" element={<MarketplacePage />} />
-            <Route path="/pricing" element={<PricingPage />} />
-            <Route path="/creators" element={<CreatorsPage />} />
-            <Route path="/blog" element={<BlogPage />} />
-            <Route path="/contact" element={<ContactPage />} />
-            <Route path="/disclaimer" element={<DisclaimerPage />} />
-            <Route path="/terms" element={<TermsPage />} />
-            <Route path="/privacy" element={<PrivacyPage />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <Navigation />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/marketplace" element={<MarketplacePage />} />
+        <Route path="/pricing" element={<PricingPage />} />
+        <Route path="/creators" element={<CreatorsPage />} />
+        <Route path="/blog" element={<BlogPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/disclaimer" element={<DisclaimerPage />} />
+        <Route path="/terms" element={<TermsPage />} />
+        <Route path="/privacy" element={<PrivacyPage />} />
+      </Routes>
+
+      <Footer />
     </Router>
   );
 }
